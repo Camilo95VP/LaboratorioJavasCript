9f280f6f03aa61d5624141b700e4bbb124264914
JuanC <camiloflstudio@gmail.com>
2022-07-15 19:47:18 -0500

    Se implementa la persistencia de datos en el localStorage

--- js/controllers.js
+++ js/controllers.js
@@ -1,28 +1,108 @@
-import { data } from '../js/base-questions.js'
+import { data } from "../js/base-questions.js";
 
+let pregunta;
+let posibles_respuestas;
+let  btn_correspondiente;
+export let usuario = {nombre: "", 
+                 puntos: 0}
 
 export const fnButton = () => {
-    const questions = document.querySelector('#pregunta');
-    questions.textContent = "cambiando ..." 
-
-}
+  const questions = document.querySelector("#pregunta");
+  questions.textContent = "cambiando ...";
+};
 
 export function escogerPreguntaAleatoria() {
-     escogerPregunta(Math.floor(Math.random() * data.length));
+  escogerPregunta(Math.floor(Math.random() * data.length));
+  //localStorage.setItem('pregunta', pregunta.pregunta);
+  // let preguntaa = localStorage.getItem('pregunta')
+  // console.log(preguntaa)
 }
 
-export function escogerPregunta(n){
-   let pregunta =data[n]
-   console.log(pregunta)
-   select_id('categoria').innerHTML = pregunta.categoria
-   select_id('pregunta').innerHTML = pregunta.pregunta
+export function escogerPregunta(n) {
+  pregunta = data[n];
+  console.log(pregunta);
+  select_id("categoria").innerHTML = pregunta.categoria;
+  select_id("pregunta").innerHTML = pregunta.pregunta;
+  desordenarRespuestas(pregunta);
+
+}
+
+
+
+export function desordenarRespuestas(pregunta) {
+  posibles_respuestas = [
+    pregunta.respuesta,
+    pregunta.incorrecta1,
+    pregunta.incorrecta2,
+    pregunta.incorrecta3,
+  ];
+
+  console.log(posibles_respuestas);
+  posibles_respuestas.sort(() => Math.random() - 0.5);
+
+  select_id("btn1").innerHTML = posibles_respuestas[0];
+  select_id("btn2").innerHTML = posibles_respuestas[1];
+  select_id("btn3").innerHTML = posibles_respuestas[2];
+  select_id("btn4").innerHTML = posibles_respuestas[3];
 }
 
 export function select_id(id) {
-    return document.getElementById(id);  
-  }
-  
+  return document.getElementById(id);
+}
+
 export function style(id) {
-    return select_id(id).style;
-  }
+  return select_id(id).style;
+}
+
+export function oprimir_btn(event) {
   
+ btn_correspondiente  = [
+    select_id("btn1"),
+    select_id("btn2"),
+    select_id("btn3"),
+    select_id("btn4"),
+  ];
+  if (posibles_respuestas[event.target.dataset.id] == pregunta.respuesta) {
+     btn_correspondiente[event.target.dataset.id].style.background = "lightgreen"
+     const score = select_id("puntaje")
+     let splitScore = score.innerHTML.split(':')
+     let myScore = parseInt(splitScore[1])+10;
+     score.innerHTML = splitScore[0]+' : ' + myScore;
+     console.log(score) 
+  } else {
+     btn_correspondiente[event.target.dataset.id].style.background = "pink" 
+
+  }
+  setTimeout(() => {
+    reiniciar()
+  }, 2000);
+
+}
+
+export function reiniciar(){
+  for(const btn of btn_correspondiente){
+    btn.style.background = 'white'
+  } 
+  escogerPreguntaAleatoria();
+}
+
+
+export function getUserLocalStorage(){
+  
+  let tempUser = JSON.parse(localStorage.getItem("usuario")) || null;
+  
+
+  if(!tempUser){
+    localStorage.setItem("usuario", JSON.stringify(usuario));
+ } else {
+   usuario = tempUser;
+ }
+ return usuario
+}
+
+//     let btns = [
+//     select_id('btn1'),
+//     select_id('btn2'),
+//     select_id('btn3'),
+//     select_id('btn4')
+// ]
--- js/game-view.js
+++ js/game-view.js
@@ -1,4 +1,4 @@
-import { escogerPregunta, escogerPreguntaAleatoria } from "../js/controllers.js"
+import { escogerPreguntaAleatoria, oprimir_btn, usuario, getUserLocalStorage} from "../js/controllers.js"
 
 export const controlView = () => {
 
@@ -29,19 +29,29 @@ export const controlView = () => {
     const containerButtons = document.createElement("div");
     containerButtons.id = "container-buttons";
 
-    
     const boton1 = document.createElement("div")
     boton1.classList.add("btn")
     boton1.id = "btn1"
+    boton1.addEventListener("click",oprimir_btn)
+    boton1.setAttribute('data-id', 0)
     const boton2 = document.createElement("div")
     boton2.classList.add("btn")
     boton2.id = "btn2"
+    boton2.addEventListener("click",oprimir_btn)
+    boton2.setAttribute('data-id', 1)
+    // boton2.addEventListener("click",oprimir_btn(1))
     const boton3 = document.createElement("div")
     boton3.classList.add("btn")
     boton3.id = "btn3"
+    boton3.addEventListener("click",oprimir_btn)
+    boton3.setAttribute('data-id', 2)
+    // boton3.addEventListener("click",oprimir_btn(2))
     const boton4 = document.createElement("div")
     boton4.classList.add("btn")
     boton4.id = "btn4"
+    boton4.addEventListener("click",oprimir_btn)
+    boton4.setAttribute('data-id', 3)
+    // boton4.addEventListener("click",oprimir_btn(3))
 
     containerButtons.append( boton1,boton2, boton3, boton4)
 
@@ -59,6 +69,11 @@ export const controlView = () => {
 
 
     escogerPreguntaAleatoria()
+    getUserLocalStorage()
 
-
+    if(usuario.nombre == "") {
+        const name = prompt('Ingresa el nombre: ')
+        usuario.nombre = name
+        localStorage.setItem('usuario', JSON.stringify(usuario));
+    }
 }
