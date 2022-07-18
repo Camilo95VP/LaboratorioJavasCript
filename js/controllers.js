f80be38240da22409d75e44421954cf8a509fad1
JuanC <camiloflstudio@gmail.com>
2022-07-17 23:50:46 -0500

    se crea el metodo reiniciar y location.reload()

--- js/controllers.js
+++ js/controllers.js
@@ -1,36 +1,44 @@
 import { data } from "../js/base-questions.js";
+/**
+ * Control general a traves de funciones para manipular la vista y correcto funcionamiento
+ * del aplicativo, teniendo en cuenta la exportacion de cada una de las funciones necesarias en la vista.
+ * @author Camilo Castañeda 
+ */
+let pregunta;              // Almacena las preguntas que vienen de la base de datos (base-questions)
+let posibles_respuestas;   // Array que almacena las 4 opciones de respuestas por cada pregunta
+let btn_correspondiente;   // Array que selecciona por id cada boton pulsado por el usuario
+let miPuntaje;             // Puntaje acumulado a lo largo del juego
+export let nombre = "";    // Nombre que sera almacenado en el LocalStorage()
+export let puntaje = 0;    // Puntaje que sera almacenado en el localStorage()
 
-let pregunta;
-let posibles_respuestas;
-let  btn_correspondiente;
-let miPuntaje;
-export let usuario = {nombre: "", 
-                 puntos: 0}
-
-export const fnButton = () => {
-  const questions = document.querySelector("#pregunta");
-  questions.textContent = "cambiando ...";
-};
-
+/**
+ * funcion encargada de seleccionar de la base de datos alguna pregunta aleatoria
+ * se hace uso de la libreria Math.random() que genera un numero aleatorio
+ */
 export function escogerPreguntaAleatoria() {
   escogerPregunta(Math.floor(Math.random() * data.length));
-  //localStorage.setItem('pregunta', pregunta.pregunta);
-  // let preguntaa = localStorage.getItem('pregunta')
-  // console.log(preguntaa)
 }
-
-export function escogerPregunta(n) {
-  pregunta = data[n];
-  console.log(pregunta);
+/**
+ * @param {int} posicionAleatoria - indice a consultar
+ * esta funcion selecciona una pregunta aleatoria, una vez seleccionada
+ * se indican las etiquetas html donde se pintaran la categoria y la pregunta
+ * dentro de esta funcion se llama otra funcion (desordenarPreguntas) encargada de
+ * desordenar las preguntas
+ */
+export function escogerPregunta(posicionAleatoria) {
+  pregunta = data[posicionAleatoria];
   select_id("categoria").innerHTML = pregunta.categoria;
   select_id("pregunta").innerHTML = pregunta.pregunta;
-  desordenarRespuestas(pregunta);
-
+  desordenarRespuestas(pregunta); 
 }
-
-
-
+/**
+ * @param {string} pregunta - parametro que recibe la pregunta,
+ * funcion que desordena las respuestas, se guardan en el array posibles_respuestas[], 
+ * luego con el metodo sort() se desordenan
+ * se selecciona cada etiqueta hatml donde se pintaran las respuestas
+ */
 export function desordenarRespuestas(pregunta) {
+
   posibles_respuestas = [
     pregunta.respuesta,
     pregunta.incorrecta1,
@@ -38,7 +46,6 @@ export function desordenarRespuestas(pregunta) {
     pregunta.incorrecta3,
   ];
 
-  console.log(posibles_respuestas);
   posibles_respuestas.sort(() => Math.random() - 0.5);
 
   select_id("btn1").innerHTML = posibles_respuestas[0];
@@ -46,82 +53,129 @@ export function desordenarRespuestas(pregunta) {
   select_id("btn3").innerHTML = posibles_respuestas[2];
   select_id("btn4").innerHTML = posibles_respuestas[3];
 }
-
+/**
+ * @param {String} id - identificacion de la etiqueta html
+ * @return {String} - getElementById()
+ * funcion encargada de seleccionar por id cada etiqueta html que correspoda
+ * de la funcion.
+ */
 export function select_id(id) {
   return document.getElementById(id);
 }
-
+/**
+ * @param {string} id - identificacion de la etiqueta html
+ * @returns {String} etiqueta seleccionada para aplicarle estilos
+ */
 export function style(id) {
   return select_id(id).style;
 }
-
+/**
+ * @param {String} event - evento que se producira al pulsar un boton,
+ * funcion encargada de verificar si la respuesta es correcta o no,
+ * si es correcta el jugador obtendra 1000 puntos,
+ * si no es correcta le restara 1000 setPuntos,
+ * recibe la funcion setPuntos() actualiza puntos
+ * recibe la funcion reiniciar() vuelve y genera una pregunta
+ * se aplica setTimeout para tener la espera de 1 segundo y luego continuar
+ */
 export function oprimir_btn(event) {
-  
- btn_correspondiente  = [
+
+  const score = select_id("puntaje");
+
+  btn_correspondiente = [                                                        //arra que obtiene los botones pulsados
     select_id("btn1"),
     select_id("btn2"),
     select_id("btn3"),
     select_id("btn4"),
   ];
-  if (posibles_respuestas[event.target.dataset.id] == pregunta.respuesta) {
-     btn_correspondiente[event.target.dataset.id].style.background = "lightgreen"
-     const score = select_id("puntaje")
-     let splitScore = score.innerHTML.split(':')
-     miPuntaje = parseInt(splitScore[1])+10;
-     score.innerHTML = splitScore[0]+' : ' + miPuntaje;
 
-  } else {
-     btn_correspondiente[event.target.dataset.id].style.background = "pink" 
-     const score = select_id("puntaje")
-     let splitScore = score.innerHTML.split(':')
-     miPuntaje = parseInt(splitScore[1])-10;
-     score.innerHTML = splitScore[0]+' : ' + miPuntaje;
+  if (posibles_respuestas[event.target.dataset.id] == pregunta.respuesta) {      //respuesta correcta
+    btn_correspondiente[event.target.dataset.id].style.background = "lightgreen";// si es correcta se pinta el boton en verde
+    let splitScore = score.innerHTML.split(":");
+    miPuntaje = parseInt(splitScore[1]) + 1000;
+    score.innerHTML = splitScore[0] + " : " + miPuntaje;
   }
+
+  if (posibles_respuestas[event.target.dataset.id] != pregunta.respuesta) {      //respuesta incorrecta
+    btn_correspondiente[event.target.dataset.id].style.background = "pink";      // si es incorrecta se pinta el boton en rojo
+    if(miPuntaje>0){
+      let splitScore = score.innerHTML.split(":");
+      miPuntaje = parseInt(splitScore[1]) - 1000;
+      score.innerHTML = splitScore[0] + " : " + miPuntaje;
+    } else if(miPuntaje<0){
+        miPuntaje = 0;
+        if(puntaje == 'undefined'){
+           puntaje = 0
+        }
+    }
+  }
+
+  setPuntos(miPuntaje);                                                           //llamado de funcion externa
+
   setTimeout(() => {
-    reiniciar()
-  }, 2000);
-
+    reiniciar();
+  }, 3000);
 }
-
-export function reiniciar(){
-  for(const btn of btn_correspondiente){
-    btn.style.background = 'white'
-  } 
+/**
+ * @param {int} puntos - puntos que se almacenaran,
+ * funcion local envia los puntos acumulados al localStorage
+ */
+function setPuntos(puntos) {
+  puntaje = puntos;
+  localStorage.setItem("puntos", puntaje);
+}
+/**
+ * reinicia el juego cada vez que se ha seleccionado una respuesta,
+ * a traves del llamado a la funcion escogerPreguntaAleatoria();
+ */
+export function reiniciar() {
+  for (const btn of btn_correspondiente) { //recorre cada boton del array y le aplica el color blanco
+    btn.style.background = "white";
+  }
   escogerPreguntaAleatoria();
 }
+/**
+ * @return {String} nombre del usuario guardado
+ * funcion que obtiene el usuario guardado en el localStorage()
+ */
+export function getUserLocalStorage() {
+  let tempUser = localStorage.getItem("nombre") || null;
+  if(!tempUser) { //verifica que el usuario exista
+    localStorage.setItem("nombre", nombre);
+    setPuntos(miPuntaje);
+  } else {
+    nombre = tempUser;
+  }
 
-
-export function getUserLocalStorage(){
-
-  let tempUser = JSON.parse(localStorage.getItem("usuario")) || null;
-  
-
-  if(!tempUser){
-    localStorage.setItem("usuario", JSON.stringify(usuario));
- } else {
-   usuario = tempUser;
- }
-
- return usuario
+  return nombre;  //nombre del usuario
 }
+/**
+ * funcion encargada de mostrar los datos que se van almacenando,
+ * como el nombre de usuario y el puntaje
+ */
+export function mostrarInfo() {
 
-export function mostrarInfo(){
+  const nJugador = select_id("info-jugador");
+  let splitJugador = nJugador.innerHTML.split(":");
+  let miNombre = localStorage.getItem("nombre");
+  nJugador.innerHTML = splitJugador[0] + " : " + miNombre;
+  const historialJugador = select_id("historial");
+  let miHistorial = localStorage.getItem("puntos");
 
-  const nJugador = select_id("info-jugador")
-  let splitJugador = nJugador.innerHTML.split(':')
-  let miNombre = usuario.nombre;
-  nJugador.innerHTML = splitJugador[0]+' : ' + miNombre;
-  
-  const historialJugador = select_id("historial")
-  let splitHistorial = historialJugador.innerHTML.split(':')
-  let miHistorial = usuario.puntos;
-  historialJugador.innerHTML = splitHistorial[0]+' : ' + miHistorial;
-
+  if (miHistorial == 'undefined') {
+    let splitHistorial = historialJugador.innerHTML.split(":");
+    historialJugador.innerHTML = splitHistorial[0] + " : " + 0;
+    puntaje = 0
+  } else {
+    let splitHistorial = historialJugador.innerHTML.split(":");
+    historialJugador.innerHTML = splitHistorial[0] + " : " + miHistorial;
+  }
 }
-
-export function cerrarSesion(){
+/**
+ * cierra la sesion, primero se limpia el local Storage y luego se reinica,
+ * para pedir de nuevo el nombre de usuario
+ */
+export function cerrarSesion() {
   localStorage.clear();
   location.reload();
 }
-
-
--- js/game-view.js
+++ js/game-view.js
@@ -1,23 +1,24 @@
-import { escogerPreguntaAleatoria, oprimir_btn, usuario, getUserLocalStorage, mostrarInfo, cerrarSesion} from "../js/controllers.js"
-
-export const controlView = () => {
-
-    //contenedor general de la app
+import { escogerPreguntaAleatoria, oprimir_btn, nombre,getUserLocalStorage, mostrarInfo, cerrarSesion} from "../js/controllers.js"
+/**
+ * vista general del aplicativo, contiene toda la estructura html,
+ * en este apartado se importan las funciones necesarias declaradas el el archivo controllers
+ * @author Camilo Castañeda 
+ */
+export const controlView = () => { //funcion que inicia la aplicacion
+    //contenedor principal de la app
     const containerApp = document.querySelector("#container-app")
 
-    //contenedor nav
+    //contenedor nav y sus elementos 
     const containerNav = document.createElement("div");
     containerNav.id = "nav";
     const category = document.createElement("h1");
     category.id = "categoria"
-
-
     const score = document.createElement("h1");
     score.id = "puntaje"
     score.textContent = "SCORE:0"
 
 
-    //contenedor pregunta
+    //contenedor de la pregunta
     const containerQuestions = document.createElement("div");
     containerQuestions.id = "cont-questions"
     const container = document.createElement("div");
@@ -25,33 +26,33 @@ export const controlView = () => {
     const questions = document.createElement("h2");
     questions.id="pregunta"
 
-
+    //Contenedor para los botones de respuestas
     const containerButtons = document.createElement("div");
     containerButtons.id = "container-buttons";
-
     const boton1 = document.createElement("div")
     boton1.classList.add("btn")
     boton1.id = "btn1"
-    boton1.addEventListener("click",oprimir_btn)
-    boton1.setAttribute('data-id', 0)
+    boton1.addEventListener("click",oprimir_btn) //evento click que llama la funcion oprimir_btn
+    boton1.setAttribute('data-id', 0)            //se settea el atributo data-id con su respectivo indice
     const boton2 = document.createElement("div")
     boton2.classList.add("btn")
     boton2.id = "btn2"
-    boton2.addEventListener("click",oprimir_btn)
-    boton2.setAttribute('data-id', 1)
+    boton2.addEventListener("click",oprimir_btn) //evento click que llama la funcion oprimir_btn
+    boton2.setAttribute('data-id', 1)            //se settea el atributo data-id con su respectivo indice
     const boton3 = document.createElement("div")
     boton3.classList.add("btn")
     boton3.id = "btn3"
-    boton3.addEventListener("click",oprimir_btn)
-    boton3.setAttribute('data-id', 2)
+    boton3.addEventListener("click",oprimir_btn) //evento click que llama la funcion oprimir_btn
+    boton3.setAttribute('data-id', 2)            //se settea el atributo data-id con su respectivo indice
     const boton4 = document.createElement("div")
     boton4.classList.add("btn")
     boton4.id = "btn4"
-    boton4.addEventListener("click",oprimir_btn)
-    boton4.setAttribute('data-id', 3)
+    boton4.addEventListener("click",oprimir_btn) //evento click que llama la funcion oprimir_btn
+    boton4.setAttribute('data-id', 3)            //se settea el atributo data-id con su respectivo indice
 
-    containerButtons.append( boton1,boton2, boton3, boton4)
-
+    containerButtons.append( boton1,boton2, boton3, boton4) //se agregan los botones al contenedor padre 
+    
+    //contenedor donde se almacenara la informacion, jugador y puntaje
     const containerInfo = document.createElement("div")
     containerInfo.id = 'container-info'
     const jugador = document.createElement("h3")
@@ -61,38 +62,43 @@ export const controlView = () => {
     historial.id = 'historial'
     historial.innerHTML = "Ultimo puntaje: " 
 
-    containerInfo.append(jugador,historial)
+    containerInfo.append(jugador,historial)//se agrega jugador y historial(puntaje)
 
-
+    //contenedor para cerrar la sesion
     const cerrarSesionContenedor = document.createElement("div")
     cerrarSesionContenedor.id = "cont-cerrar"
     const cerrarSesionBoton = document.createElement("h3")
     cerrarSesionBoton.id = "cerrar"
-    cerrarSesionBoton.innerHTML = "Cerrar sesión"
-    cerrarSesionBoton.addEventListener("click", cerrarSesion)
+    cerrarSesionBoton.innerHTML = "Borrar historial"
+    cerrarSesionBoton.addEventListener("click", cerrarSesion)//llamado a la funcion cerrarSesion
     cerrarSesionContenedor.append(cerrarSesionBoton)
 
+    //contenedor de etiqueta #hashTag
     const copyRight = document.createElement("div");
     copyRight.id = "copyR"
-    const copy = document.createElement("h4");
+    const copy = document.createElement("h6");
     copy.textContent="@QuienQuiereSerMillonario"
-    copyRight.append(copy)
+    copyRight.append(copy) //se agrega etiqueta a contendor padre
 
+    const login = document.createElement("div")
+    login.id = 'log'
+
+    //seccion donde se arma la estructura en el orden correspondiente 
     container.append(questions);
     containerQuestions.append(container, containerButtons, containerInfo);
     containerNav.append(category, score);
     containerApp.append(containerNav, containerQuestions, cerrarSesionContenedor, copyRight)
 
 
-    escogerPreguntaAleatoria()
-    getUserLocalStorage()
+    escogerPreguntaAleatoria() //funcion importada desde el archivo controllers, selecciona preguntas aleatorias
+    getUserLocalStorage()     //funcion importada desde el archivo controllers, obtiene el usuario almacenado en el localStorage
 
-    if(usuario.nombre == "") {
-        const name = prompt('Ingresa el nombre: ')
-        usuario.nombre = name
-        localStorage.setItem('usuario', JSON.stringify(usuario));
-
+    if(nombre == "") { //flujo de control para el registro de jugador
+        alert("BIENVENID@ AL JUEGO INTERACTIVO DE PREGUNTAS Y RESPUESTAS, A CONTINUACION ESCRIBE TU NOMBRE DE JUGADOR -> ")
+        const name = prompt("NOMBRE JUGADOR:")
+        let nombre = name
+        localStorage.setItem('nombre', nombre);
     }
 
-    mostrarInfo()
+    mostrarInfo()//muestra por pantalla la diferente informacion
 }
